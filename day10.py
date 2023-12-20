482b1ba078a00bbcee12359b87f61ccc8c5444cb
Jordan Barber <barberj2@cardiff.ac.uk>
2023-12-20 20:31:03 +0000

    Completed day 10

--- day10.py
+++ day10.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 
 # Make a dictionary of all moves
@@ -96,7 +97,6 @@ def CheckStep(currPos, pipe, direction, mapping=mapping, moves=moves):
         for _,look in looking.items():
             check = look(currPos)
             if check:
-                print(_, check)
                 return(check)
             
     else:
@@ -136,8 +136,87 @@ while not end:
         pipeType = moves[route[-1]]
         dirlook=pipeType.replace(dirCame,'')
 
-print(coords)
-print(route)
+# print(coords)
+# print(route)
 
 print(f'Total length of route is {len(coords)}')
 print(f'Max distance from start is {int(np.floor(len(coords)/2))}')
+
+
+###### PART B ######
+print('\n####### Part B #######\n')
+
+def PointOnEdge(x, y, p1x, p1y, p2x, p2y):
+    """
+    Check if the point (x, y) lies exactly on the edge (p1x, p1y) to (p2x, p2y).
+    """
+    if min(p1x, p2x) <= x <= max(p1x, p2x) and min(p1y, p2y) <= y <= max(p1y, p2y):
+        if (p2x - p1x) * (y - p1y) == (x - p1x) * (p2y - p1y):
+            return True
+    return False
+
+def PointInsideLoop(x, y, loop):
+    """
+    Determine if the point (x, y) is inside the given polygon.
+    Polygon is a list of tuples, each representing a vertex.
+
+    Ray-casting algorithm, essentially shooting a ray horizontally
+    from a give point, if the ray passes through an odd number of edges
+    then the origin point lies in the polygon, otherwise it is outside
+
+    returns true if point is inside loop
+    """
+    n = len(loop)
+    inside = False
+
+    p1x, p1y = loop[0] # Coords of testing point
+    for i in range(n + 1):
+        # Loop through vertices of the loop
+        p2x, p2y = loop[i % n] 
+        if PointOnEdge(x, y, p1x, p1y, p2x, p2y):
+            return False  # Exclude points on the edge
+        # Check if it crosses an edge of the loop
+        if y > min(p1y, p2y):
+            if y <= max(p1y, p2y):
+                if x <= max(p1x, p2x):
+                    # If the edge is vertical, check if left of vertex
+                    if p1y != p2y:
+                        xints = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                    if p1x == p2x or x <= xints:
+                        inside = not inside
+        p1x, p1y = p2x, p2y
+
+    return inside
+
+def CountPointsInLoop(loop):
+    """
+    Count the number of integer points inside the polygon.
+    """
+    # Find the bounding box (reduces number to check through)
+    min_x = min(loop, key=lambda p: p[0])[0]
+    max_x = max(loop, key=lambda p: p[0])[0]
+    min_y = min(loop, key=lambda p: p[1])[1]
+    max_y = max(loop, key=lambda p: p[1])[1]
+
+    # Count points in the polygon
+    count = 0
+    insideCoords=[]
+    for x in range(min_x, max_x + 1):
+        for y in range(min_y, max_y + 1):
+            if PointInsideLoop(x, y, loop):
+                count += 1
+                insideCoords.append((x,y))
+            
+            print(f'x search {x/(max_x+1):.0%} --- y search {y/(max_y+1):.0%}', end='\r')
+
+    return count, insideCoords
+
+numPoints, insideCoords = CountPointsInLoop(coords)
+print(f'There are {numPoints} inside of the loop')
+
+x, y = zip(*coords)
+inx, iny = zip(*insideCoords)
+
+plt.plot(x, y)
+plt.plot(inx, iny, 'x', color='black')
+plt.show()
